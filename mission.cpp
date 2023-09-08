d5765e5a17f53ee3ef92240b9c3e13ad179d2720
skippy2004 <56gaming56@gmail.com>
2023-09-08 11:08:30 +0200

    nouveau fichier mission

---
+++ mission.cpp
@@ -0,0 +1,138 @@
+/*****************************************************************//**
+ * \file   Entreprise.cpp
+ * \brief  Definition de la classe Entreprise
+ *
+ * \author PierreV
+ * \version 0.1
+ * \date   June 2023
+ *
+ * Programme Cpp permettant de creer des objets pour décrire les stages
+ *********************************************************************/
+
+#include "mission.h"
+
+
+
+using namespace std;
+
+
+/**
+ * \brief Constructeur par default
+ * \li Nom = anonyme
+ * \li Ville = inconnu
+ *
+ */
+Mission::Mission() {
+
+    this->descriptifMission = "";
+    this->entrepriseMission = new Entreprise();
+    this->nbPersonne = 0;
+    this->nbHeure = 0;
+
+}
+
+/**
+ * \brief Constructeur avec parametres rentres en argument
+ *
+ * \param nom nom de l'entreprise
+ * \param ville ville de l'entreprise
+ */
+//Entreprise::Entreprise(std::string nom, std::string ville) {
+//    this->nomEntreprise = nom;
+//
+//    this->villeEntreprise = ville;
+//}
+
+
+/**
+ * \brief Destructeur
+ *
+ */
+Mission::~Mission() {
+
+    cout << "Destrcution de la mission " << endl;
+
+}
+
+/**
+ * \brief Constructeur de copie
+ *
+ * \param entre Objet de type entreprise
+ */
+//Entreprise::Entreprise(const Entreprise& entre)
+//{
+//    cout << "Copie de Capitaine" << endl;
+//    this->nomEntreprise = entre.nomEntreprise;
+//    this->villeEntreprise = entre.villeEntreprise;
+//
+//}
+
+/**
+ * \brief Operateur d'affectation
+ *
+ * \param entre Objet de type entreprise
+ * \return Nouvelle objet de type entreprise
+ */
+//Entreprise& Entreprise::operator = (const Entreprise& entre)
+//{
+//    cout << "Affectation de l'entreprise" << std::endl;
+//    if (this != &entre)  // Pour éviter que l'on ne se recopie pas...
+//    {
+//        this->nomEntreprise = entre.nomEntreprise;
+//        this->villeEntreprise = entre.villeEntreprise;
+//   }
+//   return *this;
+//}
+
+int Mission::getNbPersonne() {
+    return this->nbHeure;
+}
+
+void  Mission::setNbPersonne(int nbPersonnev) {
+    this->nbPersonne = nbPersonnev;
+}
+
+
+
+
+
+
+
+
+std::string Mission::getDescriptifMiossion()
+{
+    return this->descriptifMission;
+}
+
+void Mission::setDescriptifMiossion(string descriptifMissionv) {
+    this->descriptifMission = descriptifMissionv;
+}
+
+
+
+
+
+
+
+
+int Mission::getNbHeure() {
+    return this->nbHeure;
+}
+
+void  Mission::setNbHeure(int nbHeurev) {
+    this->nbHeure = nbHeurev;
+}
+
+
+
+
+
+
+
+Entreprise* Mission::getEntrepriseMission() {
+    return this-> entrepriseMission;
+}
+
+void  Mission::setEntrepriseMission(string *entrepriseMission) {
+    this-> entrepriseMission = *entrepriseMission;
+}
---
+++ mission.h
@@ -0,0 +1,68 @@
+/*****************************************************************//**
+ * \file   mission.h
+ * \brief  Declaration de la classe entreprise
+ *
+ * \author Pierre
+ * \date   June 2023
+ *********************************************************************/
+
+#ifndef MISSION_H
+#define MISSION_H
+#include <iostream>
+#include <string>
+#include "Entreprise.h"
+
+using namespace std;
+
+
+ /*! \class Entreprise
+    * \brief classe representant une entreprise. 2 attributs prives
+    *
+    * Cette classe a une finalite pedagogique :
+    * \li : Rappel du C++
+    * \li : commentaire avec Doxygen
+    * \li : git, github
+    */
+class Mission
+{
+
+private:
+    /**
+     * Membre prive nb Personne : contient le nombre de personne
+     */
+    int nbPersonne;
+    /**
+     * Membre prive descriptifMission : contient les descriptif des mission
+     */
+    string descriptifMission;
+    /**
+     * Membre prive nbHeure : contient le nombre d'heure
+     */
+    int nbHeure;
+
+    Entreprise *entrepriseMission;
+
+protected:
+    
+    
+
+public:
+    /*Classe sous forme canonique*/
+    Mission();
+    ~Mission();
+    
+    int getNbPersonne();
+    void setNbPersonne(int nbPersonnev);
+
+    string getDescriptifMiossion();
+    void setDescriptifMiossion(string descriptifMissionv);
+
+    int getNbHeure();
+    void setNbHeure(int nbHeurev);
+
+    Entreprise* getEntrepriseMission();
+    void setEntrepriseMission(string *entrepriseMission);
+
+};
+
+#endif
